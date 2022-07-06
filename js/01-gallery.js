90623dc5cd18822dfc909c6c3330398cf50e8263
VKlapan <vladimir.klapan@gmail.com>
2022-07-06 20:33:41 +0300

    add modal window task#01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,8 +8,8 @@ const galleryMarkupMaker = (galleryItems) => {
     .map((item) => {
       return `
         <li class="gallery__item">
-        <a class="gallery__link" href="${item.original}">
-        <img class="gallery__image" src="${item.preview}">
+        <a class="gallery__link" href="${item.description}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-source="${item.original}">
         </a>
         </li>
         `;
@@ -19,5 +19,15 @@ const galleryMarkupMaker = (galleryItems) => {
   return `<ul class=gallery> ${itemLiMarkup} </ul>`;
 };
 
+const modalWindowShow = (event) => {
+  event.preventDefault();
+  const selectedImgSource = event.target.dataset.source;
+  const instance = basicLightbox.create(`<img src="${selectedImgSource}">`);
+  instance.show();
+};
+
 galleryEl.insertAdjacentHTML("afterbegin", galleryMarkupMaker(galleryItems));
+
+galleryEl.addEventListener("click", modalWindowShow);
+
 //console.log(galleryMarkupMaker(galleryItems));
