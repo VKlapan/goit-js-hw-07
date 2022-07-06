878aedcd408d6350a55beba50d06da67cbcaf7c2
VKlapan <vladimir.klapan@gmail.com>
2022-07-06 16:09:42 +0300

    add gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMarkupMaker = (galleryItems) => {
+  const itemLiMarkup = galleryItems
+    .map((item) => {
+      return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}">
+        </a>
+        </li>
+        `;
+    })
+    .join("");
+
+  return `<ul class=gallery> ${itemLiMarkup} </ul>`;
+};
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkupMaker(galleryItems));
+//console.log(galleryMarkupMaker(galleryItems));
