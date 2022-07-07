0c3d4ecda367f36a637f133f640a872577364648
VKlapan <vladimir.klapan@gmail.com>
2022-07-07 15:59:04 +0300

    fix img original src

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,7 +8,7 @@ const createGalleryMarkup = (galleryItems) => {
     .map((item) => {
       return `
         <li class="gallery__item">
-        <a class="gallery__link" href="${item.description}9">
+        <a class="gallery__link" href="${item.original}">
         <img class="gallery__image" src="${item.preview}"
         alt="${item.description}"
         data-source="${item.original}">
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const createGalleryMarkup = (galleryItems) => {
+  const itemLiMarkup = galleryItems
+    .map((item) => {
+      return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}"
+        alt="${item.description}"
+        data-source="${item.original}">
+        </a>
+        </li>
+        `;
+    })
+    .join("");
+
+  return `<ul class=gallery> ${itemLiMarkup} </ul>`;
+};
+
+galleryEl.insertAdjacentHTML("afterbegin", createGalleryMarkup(galleryItems));
