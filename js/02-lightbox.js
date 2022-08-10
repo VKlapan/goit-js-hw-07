2838246bbb20ab664ba44054b1af18052e28d755
VKlapan <vladimir.klapan@gmail.com>
2022-08-10 18:06:39 +0300

    refactoring gallery#02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,32 +1,30 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryEl = document.querySelector(".gallery");
+(function () {
+  const galleryEl = document.querySelector(".gallery");
 
-const createGalleryMarkup = (galleryItems) => {
-  const itemLiMarkup = galleryItems
-    .map((item) => {
-      return `
-      <li>
+  const renderGalleryElement = (item) => {
+    return `<li>
         <a class="gallery__item" href="${item.original}">
             <img  class="gallery__image"
                 src="${item.preview}"
                 alt="${item.description}">
         </a>
       </li>
-        `;
-    })
-    .join("");
+      `;
+  };
 
-  return itemLiMarkup;
-};
+  const createGalleryMarkup = (galleryItems) =>
+    galleryItems.map(renderGalleryElement).join("");
 
-galleryEl.insertAdjacentHTML("afterbegin", createGalleryMarkup(galleryItems));
+  galleryEl.insertAdjacentHTML("afterbegin", createGalleryMarkup(galleryItems));
 
-var lightbox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionPosition: "bottom",
-  captionDelay: 250,
-});
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
 
-lightbox.on("show.simplelightbox");
+  lightbox.on("show.simplelightbox");
+})();
